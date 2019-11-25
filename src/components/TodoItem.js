639eaa37003062e445804eae9b3a5cb2757b134b
Je Vi Tan <jevitan@Jes-MacBook-Pro-2.local>
2019-11-24 20:58:26 -0600

    List the tasks

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,10 +3,19 @@ import React, { Component } from 'react'
 
 export default class TodoItem extends Component {
     render() {
+        const {title, handleDelete, handleEdit} = this.props
         return (
-            <div>
-                <h3>Hello from todoItem.js</h3>
-            </div>
+            <li className="list-group-item text-capitalize d-flex justify-content-between my-1.5">
+                <h6>{title}</h6>
+                <div>
+                    <span className="mx-2 text-success" onClick={handleEdit} >
+                        <i className="fas fa-pen"></i>
+                    </span>
+                    <span className="mx-2 text-danger" onClick={handleDelete} >
+                        <i className="fas fa-eraser"></i>
+                    </span>
+                </div>
+            </li>
         )
     }
 }
